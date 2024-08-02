3baabc932ccba2d813cbbde0baa051f086e77af3
Amritesh VB <57042153+amriteshamr@users.noreply.github.com>
2024-08-02 23:59:04 +0200

    Add files via upload

---
+++ check_vowel.py
@@ -0,0 +1,12 @@
+#Check whether a character is vowel or not.
+
+vow = ['a','e','i','o','u']
+
+ch = input("Enter a Character : ").lower()
+
+if ch not in vow:
+    print("Not Vowel")
+else:
+    print("Vowel")
+
+    
